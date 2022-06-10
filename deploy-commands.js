0c4e5bc5a61b9652ba70ef94fc4f609a7e0b76e6
rnbsov <73550760+rnbsov@users.noreply.github.com>
2022-06-11 02:46:59 +0600

    👾 deploy to heroku

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,8 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
+const { SlashCommandBuilder, SlashCommandSubcommandBuilder } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routers } = require('discord-api-types/v9')
+const { clientId, guildId, token } = require('./config.json')
 
+const commands = [
+  new 
+]
